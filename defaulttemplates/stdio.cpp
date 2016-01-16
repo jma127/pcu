2befcdd45514d29b6110bbb00d2de2bc17f48f44
Jerry Ma <jerryma@nospam>
2016-01-16 17:59:15 -0500

    fix c++ templates

--- defaulttemplates/stdio.cpp
+++ defaulttemplates/stdio.cpp
@@ -3,37 +3,75 @@
  * $SRCFILE
  */
 
-#include <bits/stdc++.h>
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <chrono>
+#include <complex>
+#include <deque>
+#include <forward_list>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
 typedef long long int lli;
 typedef pair<int, int> pii;
+typedef pair<int, lli> pil;
+typedef pair<lli, int> pli;
 typedef pair<lli, lli> pll;
 
 int gInt () {
-    int i;
-    scanf("%d", &i);
-    return i;
+  int i;
+  scanf("%d", &i);
+  return i;
 }
 
 lli gLong () {
-    lli i;
-    scanf("%lld", &i);
-    return i;
+  lli i;
+  scanf("%lld", &i);
+  return i;
 }
 
 double gDouble () {
-    double i;
-    scanf("%lf", &i);
-    return i;
+  double i;
+  scanf("%lf", &i);
+  return i;
 }
 
 void quit () {
-    fflush(stdout);
-    exit(0);
+  fflush(stdout);
+  exit(0);
 }
 
 int main (int argc, char ** argv) {
-    quit();
+  quit();
 }
